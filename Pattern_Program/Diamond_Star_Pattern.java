09cfa4ca927882ec40c045031a71cca917345c4b
Saood Ahmad <saoodchoudhary9@gmail.com>
2022-08-15 02:48:59 +0530

    update diamond pattern

--- Pattern_Program/Diamond_Star_Pattern.java
+++ Pattern_Program/Diamond_Star_Pattern.java
@@ -6,13 +6,13 @@ public class Diamond_Star_Pattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         /*
+         *
          * *
-         * * *
-         * * *
-         * * *
-         * * *
-         * * *
          * *
+         * *
+         * *
+         * *
+         *
          */
         System.out.println("Enter the number of rows you want to print: ");
         int rows = sc.nextInt();
